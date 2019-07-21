964a5731edfa09c64ef571bab2e2d4eb1a8bacb3
cutedayoung <dayoung7406@gmail.com>
2019-07-20 18:37:31 -0700

    add css

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 var express = require('express'); // module을 require 명령어를 통해 호출
-
+var path = require('path');
 var app = express(); // 함수를 app 변수에 담음, express()는 생성자 개념
 
-app.use(express.static(__dirname + '/public')); // 미들함수
+// app.use(express.static(__dirname + '/public')); // 미들함수
+
+// __dirname은 node에서 제공하는 node 파일의 경로를 담고 있는 변수
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/',function (req,res){
   res.send('Hellow World!');
@@ -11,4 +15,5 @@ app.get('/',function (req,res){
 
 app.listen(3000, function(){
   console.log('Server On!');
-});
+}); // 두개의 argument를 받는데, 첫번째는 port num / 두번째는 fun 3000번으로 들어오는
+// 신호를 감지하 반응, 두번째는 바로 실행, console 창을 띄우기 위해서 사용
